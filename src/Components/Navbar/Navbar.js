6a341d383aaa430a62145395eb807c63e7a6dd8a
Abram <abramarruda@gmail.com>
2018-11-18 21:19:21 -0500

    changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Fade from "react-reveal/Fade";
 import Logo from "../../images/LOGO_B.png";
 import Logo2 from "../../images/LOGO.png";
 import { Link } from "react-router-dom";
@@ -9,53 +8,45 @@ import "./Navbar.css";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-md">
+      <nav class="navbar navbar-expand-md justify-content-left">
         <div>
-          <Fade duration={2000} delay={300}>
-            <Link
-              class="nav-link"
-              to={process.env.PUBLIC_URL + "/"}
-              id="logoLink"
-            >
-              <img src={Logo} id="fullImage" />
-              <img src={Logo2} id="mobileImage" />
-            </Link>
-          </Fade>
+          <Link
+            class="nav-link"
+            to={process.env.PUBLIC_URL + "/"}
+            id="logoLink"
+          >
+            <img src={Logo} id="fullImage" />
+            <img src={Logo2} id="mobileImage" />
+          </Link>
         </div>
 
         <ul class="nav">
           <li class="nav-item">
-            <Fade duration={2000} delay={300}>
-              <Link
-                class="nav-link underline"
-                to={process.env.PUBLIC_URL + "/AboutMe"}
-                id="aboutLink"
-              >
-                about
-              </Link>
-            </Fade>
+            <Link
+              class="nav-link underline"
+              to={process.env.PUBLIC_URL + "/AboutMe"}
+              id="aboutLink"
+            >
+              about
+            </Link>
           </li>
           <li class="nav-item">
-            <Fade duration={2000} delay={300}>
-              <Link
-                class="nav-link underline"
-                to={process.env.PUBLIC_URL + "/Works"}
-                id="worksLink"
-              >
-                works
-              </Link>
-            </Fade>
+            <Link
+              class="nav-link underline"
+              to={process.env.PUBLIC_URL + "/Works"}
+              id="worksLink"
+            >
+              works
+            </Link>
           </li>
           <li class="nav-item">
-            <Fade duration={2000} delay={300}>
-              <Link
-                class="nav-link underline"
-                to={process.env.PUBLIC_URL + "/Contact"}
-                id="contactLink"
-              >
-                contact
-              </Link>
-            </Fade>
+            <Link
+              class="nav-link underline"
+              to={process.env.PUBLIC_URL + "/Contact"}
+              id="contactLink"
+            >
+              contact
+            </Link>
           </li>
         </ul>
       </nav>
