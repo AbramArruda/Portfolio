b52abda964d0c4a0624f0d2622cda7812e0f4f39
Abram <abramarruda@gmail.com>
2018-11-18 22:36:09 -0500

    changes

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -15,7 +15,7 @@ export default class AboutMe extends Component {
                 <h1 id="aboutMeHeader">
                   {" "}
                   I am looking to better my current skills and contially learn.{" "}
-                  <Fade delay={6500}>
+                  <Fade delay={5500}>
                     <a
                       download="Resume.pdf"
                       href={Resume}
@@ -37,7 +37,7 @@ export default class AboutMe extends Component {
               </Fade>
             </div>
             <div class="col-sm-12 col-lg-8 no-padding">
-              <Fade bottom duration={1800} delay={2000}>
+              <Fade bottom duration={1800} delay={1000}>
                 <p id="bio">
                   I am a Web Developer, currently living in the Atlanta area.
                   Recent graduate of a full stack web development bootcamp
@@ -57,17 +57,17 @@ export default class AboutMe extends Component {
           </div>
           <div class="row about-row" id="aboutMeSkills">
             <div class="col-sm-12 col-lg-4 no-padding">
-              <Fade left delay={500}>
+              <Fade left delay={200}>
                 <h2 id="skills">Skills:</h2>
               </Fade>
             </div>
             <div class="col-sm-12 col-lg-6">
               <div class="row" id="skillsWrapper">
-                <Fade bottom cascade duration={2500} delay={2500}>
+                <Fade bottom cascade duration={2500} delay={1500}>
                   <div class="col-3" id="frontEndSkills">
                     <h3 id="frontEnd"> Front-End: </h3>
 
-                    <Fade bottom cascade delay={4000}>
+                    <Fade bottom cascade delay={2000}>
                       <ul id="skillsList1">
                         <li>Html</li>
                         <li>CSS</li>
@@ -79,7 +79,7 @@ export default class AboutMe extends Component {
                   <div class="col-3" id="backEndSkills">
                     <h3 id="backEnd"> Back-End: </h3>
 
-                    <Fade bottom cascade delay={4000}>
+                    <Fade bottom cascade delay={2000}>
                       <ul id="skillsList2">
                         <li>MySQL</li>
                         <li>MongoDB</li>
@@ -90,7 +90,7 @@ export default class AboutMe extends Component {
                   </div>
                   <div class="col-3" id="devToolsSkills">
                     <h3 id="devTools"> Dev Tools: </h3>
-                    <Fade bottom cascade delay={4000}>
+                    <Fade bottom cascade delay={2000}>
                       <ul id="skillsList3">
                         <li>NPM/Yarn</li>
                         <li>O-Auth</li>
@@ -106,16 +106,16 @@ export default class AboutMe extends Component {
 
           <div class="row about-row">
             <div class="col-sm-12 col-lg-4 no-padding" id="aboutMeEducation">
-              <Fade left delay={1000}>
+              <Fade left delay={400}>
                 <h2 id="education">Education:</h2>
               </Fade>
             </div>
             <div class="col-sm-5 col-lg-6">
               <div class="row" id="educationWrapper">
-                <Fade bottom cascade duration={2500} delay={3000}>
+                <Fade bottom cascade duration={2000} delay={1700}>
                   <div class="col-sm-5 no-padding">
                     <h3 id="degree"> Bachelors Degree in Biology </h3>
-                    <Fade bottom cascade delay={4000}>
+                    <Fade bottom cascade delay={2000}>
                       <ul>
                         <li>Spring 2013 - December 2017</li>
                         <li>University of West Georgia</li>
@@ -124,7 +124,7 @@ export default class AboutMe extends Component {
                   </div>
                   <div class="col-5 no-padding">
                     <h3 id="certificate"> Full Stack Development Program </h3>
-                    <Fade bottom cascade delay={4000} appear={true}>
+                    <Fade bottom cascade delay={2000} appear={true}>
                       <ul>
                         <li>May 2018 - November 2018</li>
                         <li>Georiga Institute of Technology</li>
