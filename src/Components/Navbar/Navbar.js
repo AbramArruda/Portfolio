0ebb4fe7f68b8cf9111fbf0bd792a03e2463a19b
Abram <abramarruda@gmail.com>
2018-11-20 15:42:31 -0500

    changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Logo from "../../images/LOGO_B.png";
 import Logo2 from "../../images/LOGO.png";
 import { Link } from "react-router-dom";
+import Flip from "react-reveal/Flip";
 
 import "./Navbar.css";
 
@@ -10,16 +11,17 @@ export default class Navbar extends Component {
     return (
       <nav class="navbar navbar-expand-md justify-content-left" id="navbar">
         <div>
-          <Link
-            class="nav-link"
-            to={process.env.PUBLIC_URL + "/"}
-            id="logoLink"
-          >
-            <img src={Logo} id="fullImage" />
-            <img src={Logo2} id="mobileImage" />
-          </Link>
+          <Flip duration={1500} delay={500}>
+            <Link
+              class="nav-link"
+              to={process.env.PUBLIC_URL + "/"}
+              id="logoLink"
+            >
+              <img src={Logo} id="fullImage" />
+              <img src={Logo2} id="mobileImage" />
+            </Link>
+          </Flip>
         </div>
-
         <ul class="nav" id="linkContainer">
           <li class="nav-item">
             <Link
